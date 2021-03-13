2f198b28aed012aeeaf93a646db644e5cda513a4
Chris <chris.mahfouz@hotmail.com>
2021-03-13 09:30:34 +0000

    Automatically backed up by Learn

--- konami_code.js
+++ konami_code.js
@@ -13,4 +13,20 @@ const codes = [
 
 function init() {
   // your code here
+  document.body.addEventListener('keydown',function(event){
+    if(event.codes===["ArrowUp",
+    "ArrowUp",
+    "ArrowDown",
+    "ArrowDown",
+    "ArrowLeft",
+    "ArrowRight",
+    "ArrowLeft",
+    "ArrowRight",
+    "b",
+    "a"]){
+      alert("congratulatory message")
+    }else{
+      console.log()
+    }
+  })
 }
